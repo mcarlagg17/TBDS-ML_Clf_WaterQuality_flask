98d993abe583f4aae918dc292d698bf8bf785477
mcarlagg17 <carla.glezz@gmail.com>
2022-09-14 16:53:21 +0200

    commit prueba 2

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+import pygit as git
 
 app = Flask(__name__)
+
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./TBDS_ML_Clf_WaterQuality_flask')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
 
 @app.route("/", methods=['GET'])
 def home():
