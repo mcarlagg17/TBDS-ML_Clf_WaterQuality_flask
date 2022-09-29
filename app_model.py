c739a186dd035eb787aa2491930f79210bc0e741
mcarlagg17 <carla.glezz@gmail.com>
2022-09-30 01:42:22 +0200

    fondo 12

--- app_model.py
+++ app_model.py
@@ -59,10 +59,10 @@ def predict():
     if file and allowed_file(file.filename):
         datos = pd.read_csv(file)
         tamano = len(datos)
-        #scaler = pickle.load(open('scaler.pkl', 'rb'))
+        scaler = pickle.load(open('scaler.pkl', 'rb'))
         model = pickle.load(open('model_selected.pkl', 'rb'))
-        #datos_scaled = scaler.transform(datos)
-        prediction = model.predict(datos)
+        datos_scaled = scaler.transform(datos)
+        prediction = model.predict(datos_scaled)
         
         if tamano>1:
             tabla = pd.DataFrame(datos,columns=datos.columns)
